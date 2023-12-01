1761d07de7ef4f91ed38abcc61b9001c7bc391b7
jillianjoy1 <jillianjoy810@gmail.com>
2023-11-30 19:30:39 -0800

    Added bootstrap and aligned weather dates

--- src/index.js
+++ src/index.js
@@ -16,12 +16,27 @@ function refreshWeather(response) {
   windSpeedElement.innerHTML = `${response.data.wind.speed}mph`;
   temperatureElement.innerHTML = Math.round(temperature);
   icon.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
+  console.log(response.data);
+  getForecast(response.data.coordinates);
+}
+
+function getForecast(coordinates) {
+  let apiKey = "aca4dd3643b89e94dbd3cac6cf6f2638";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.latitude}&lon=${coordinates.longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
 }
 
 function formatDate(date) {
   let hours = date.getHours();
   let minutes = date.getMinutes();
-
 
   let days = [
     "Sunday",
@@ -33,7 +48,6 @@ function formatDate(date) {
     "Saturday",
   ];
   let day = days[date.getDay()];
-
 
   if (minutes < 10) {
     minutes = `0${minutes}`;
@@ -55,40 +69,45 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
-
-function displayForecast() {
- 
-
-  let days = ["Tues", "Wed", "Thurs", "Fri", "Sat"];
-  let forecastHtml ="";
-
-  days.forEach(function (day) {
-    forecastHtml = forecastHtml +
-    `
-<div class="weather-forecast-day">
-          <div class="weather-forecast-date">${day}</div>
-          <div class="weather-forecast-icon">🌤️</div>
-          <div class="weather-forecast-temperatures">
-            <div class="weather-forecast-temperature">
-              <strong>15°</strong>
-            </div>
-          <div class="weather-forecast-temperature">9°</div>
-          </div>
-        </div>
-        `;
-  });
+function displayForecast(response) {
+  let forecast = response.data.daily;
 
   let forecastElement = document.querySelector("#forecast");
-  forecastElement.innerHTML = forecastHtml;
+
+  let forecastHTML = `<div class="row">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
+      <div class="col">
+        <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+        <img
+          src="http://openweathermap.org/img/wn/${
+            forecastDay.weather[0].icon
+          }@2x.png"
+          alt=""
+          width="42"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> ${Math.round(
+            forecastDay.temp.max
+          )}° </span>
+          <span class="weather-forecast-temperature-min"> ${Math.round(
+            forecastDay.temp.min
+          )}° </span>
+        </div>
+      </div>
+  `;
+    }
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Portland");
-displayForecast();
-
-
-
-
-
+// displayForecast();
